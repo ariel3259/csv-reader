39840c36c6ce5d2d9e14afc6f4d88390cdbbb0e3
Ariel_Santangelo <78393362+ariel3259@users.noreply.github.com>
2023-05-08 10:14:46 -0300

    change parseInt to Number in cast function

--- utils/cast.ts
+++ utils/cast.ts
@@ -1,12 +1,12 @@
 export const cast = (type: string, payload: string): string | number | boolean => {
     const types: Record<string, any> = {
-        number: (): number => parseInt(payload),
+        number: (): number => Number(payload),
         boolean: (): boolean => payload === 'TRUE',
         date: (): Date => {
             const metadata: string[] = `${payload}`.split('-');
-            const year: number = parseInt(metadata[0]);
-            const month: number = parseInt(metadata[1]) - 1;
-            const day: number = parseInt(metadata[2]);
+            const year: number = Number(metadata[0]);
+            const month: number = Number(metadata[1]) - 1;
+            const day: number = Number(metadata[2]);
             return new Date(year, month, day);
         },
     }
